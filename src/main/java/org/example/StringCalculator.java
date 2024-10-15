6d15b7d4357e0eccebdb2c887adc913829a8a905
Ranjithkumar Alampally <ranjithkumar_alampally@epam.com>
2024-10-16 02:10:40 +0530

    Fixed Initial Testcases

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -4,6 +4,16 @@ public class StringCalculator {
 
     public int add(String numbers)
     {
-        return 0;
+        int result=0;
+        if(numbers.isEmpty())
+        {
+            return 0;
+        }
+
+        for(String num:numbers.split(","))
+        {
+            result+= Integer.parseInt(num);
+        }
+        return result;
     }
 }
